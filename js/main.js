6b8a872de36d19d23117b3f702d2d188d97869d9
Ozielc4 <ozielc4@gmail.com>
2023-04-18 16:57:54 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const dropArea = document.querySelector(".drag-files")
+
+dropArea.addEventListener("dragover", () => {
+  dropArea.classList.add("dragover")
+})
+
+dropArea.addEventListener("dragleave", () => {
+  dropArea.classList.remove("dragover")
+})
+
+console.log('ola')
